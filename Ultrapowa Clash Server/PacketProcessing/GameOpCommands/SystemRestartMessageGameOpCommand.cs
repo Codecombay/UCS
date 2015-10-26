346b54078cbce5ffb3461538312e3076c7a03a5a
Berkan YILDIZ <admin@gobelinland.fr>
2015-10-26 21:42:57 +0100

    Beta 1

---
+++ Ultrapowa Clash Server/PacketProcessing/GameOpCommands/SystemRestartMessageGameOpCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using UCS.Logic;
+using UCS.Helpers;
+using UCS.GameFiles;
+using UCS.Core;
+using UCS.Network;
+
+namespace UCS.PacketProcessing
+{
+    class SystemRestartMessageGameOpCommand : GameOpCommand
+    {
+        private string[] m_vArgs;
+
+        public SystemRestartMessageGameOpCommand(string[] args)
+        {
+            m_vArgs = args;
+            SetRequiredAccountPrivileges(4);
+        }
+
+        public override void Execute(Level level)
+        {
+            if (level.GetAccountPrivileges() >= GetRequiredAccountPrivileges())
+            {
+                if (m_vArgs.Length >= 1)
+                {
+                    var avatar = level.GetPlayerAvatar();
+                    AllianceMailStreamEntry mail = new AllianceMailStreamEntry();
+                    mail.SetId((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds);
+                    mail.SetSenderId(0);
+                    mail.SetSenderAvatarId(0);
+                    mail.SetSenderName("System Admin");
+                    mail.SetIsNew(0);
+                    mail.SetAllianceId(0);
+                    mail.SetAllianceBadgeData(0);
+                    mail.SetAllianceName("System Manager");
+                    mail.SetMessage("System is restarting in a few moments");
+                    mail.SetSenderLevel(500);
+                    mail.SetSenderLeagueId(22);
+
+                    foreach (var onlinePlayer in ResourcesManager.GetOnlinePlayers())
+                    {
+                        var p = new AvatarStreamEntryMessage(onlinePlayer.GetClient());
+                        p.SetAvatarStreamEntry(mail);
+                        PacketManager.ProcessOutgoingPacket(p);
+                        Console.WriteLine("issue");
+                    }
+                    System.Diagnostics.Process.Start(@"tools\ucs-restart.bat");
+                }
+            }
+            else
+            {
+                SendCommandFailedMessage(level.GetClient());
+            }
+        }
+    }
+}
---
+++ Ultrapowa Clash Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Configuration;
+using System.Net;
+using UCS.Network;
+using UCS.PacketProcessing;
+using UCS.Core;
+
+namespace UCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ultrapowa Clash Server v0.6.1.1 - Beta 1";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(
+@"
+888     888  .d8888b.   .d8888b.  
+888     888 d88P  Y88b d88P  Y88b 
+888     888 888    888 Y88b.      
+888     888 888         ""Y888b.   
+888     888 888            ""Y88b. 
+888     888 888    888       ""888 
+Y88b. .d88P Y88b  d88P Y88b  d88P 
+ ""Y88888P""   ""Y8888P""   ""Y8888P""  
+        ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ultrapowa Clash Server");
+            Console.WriteLine("version 0.6.1.1 - Beta 1");
+            Console.WriteLine("www.ultrapowa.com");
+            Console.WriteLine("www.gobelinland.fr");
+            Console.WriteLine("Server starting...");
+            string hostName = Dns.GetHostName();
+            string myIP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+            Gateway g = new Gateway();
+            PacketManager ph = new PacketManager();
+            MessageManager dp = new MessageManager();
+            ResourcesManager rm = new ResourcesManager();
+            ObjectManager pm = new ObjectManager();
+            dp.Start();
+            ph.Start();
+            g.Start();
+            if (Convert.ToBoolean(ConfigurationManager.AppSettings["apiManager"]))
+            {
+                ApiManager api = new ApiManager();
+                Debugger.SetLogLevel(Int32.Parse(ConfigurationManager.AppSettings["loggingLevel"]));
+                Logger.SetLogLevel(Int32.Parse(ConfigurationManager.AppSettings["loggingLevel"]));
+                Console.WriteLine("Server started on " + myIP + ":9339 and let's play Clash of Clans!");
+                Thread.Sleep(Timeout.Infinite);
+            }
+            else
+            {
+                Console.WriteLine("Api Manager disabled...");
+                Debugger.SetLogLevel(Int32.Parse(ConfigurationManager.AppSettings["loggingLevel"]));
+                Logger.SetLogLevel(Int32.Parse(ConfigurationManager.AppSettings["loggingLevel"]));
+                Console.WriteLine("Server started on " + myIP + ":9339 and let's play Clash of Clans!");
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
